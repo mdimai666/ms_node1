7f709ff64475c7c8d8406b79aefa0e63cdf4c5ee
mdimai666 <mdimai666@mail.ru>
2020-01-09 10:07:30 +0900

    fix pm2 reduce size

--- docker-run.ps1
+++ docker-run.ps1
@@ -1,7 +1,7 @@
 $host.UI.RawUI.BackgroundColor = "Blue"
 $host.UI.RawUI.ForegroundColor = "Red"
 Write-Output 'RUN CONTAINER node1'
-docker rm node1 -f
+# docker rm mdimai666/node1:latest -f
 docker run --rm -d -p 3001:3001/tcp --name node1 mdimai666/node1:latest
 [Console]::ResetColor()
 Write-Output ' '
